1f8b4e492cf448e57471b563d55e785a0fb05be8
Lorenz Pircher <h1633083@wu.ac.at>
2022-11-03 00:01:12 +0100

    Full Docker Support

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Dokumentation folgt ...
 
 > Zum Ausführen benötigt man [Factory I/O](https://factoryio.com/start-trial), [.NET](https://dotnet.microsoft.com/en-us/download/dotnet/5.0) Runtime sowie SDK und [Docker](https://www.docker.com/). Um auf die Endpoints von der REST API zugreifen zu können, benötigt man eine SSH Verbindung mit http://abgabe.cs.univie.ac.at .
 ```
-<!-- In der Konsole einfügen: -->
+<!-- 1. Prüfen, auf welchem TCP Port Factory I/O den Modbus Server belegt, bei Bedarf im setup.sh file aktualisieren. -->
+<!-- 2. In der Konsole einfügen: -->
 cmd /k setup.bat
+<!-- 3. .st File in OpenPLC hochladen und starten -->
 ```
--- openplc/setup.sh
+++ openplc/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SQL_DEVICE="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('FactoryIO', 'TCP', 1, '127.0.0.1', 503, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8);"
+SQL_DEVICE="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('FactoryIO', 'TCP', 1, '192.168.1.2', 503, 0, 16, 0, 16, 0, 8, 0, 8, 0, 8);"
 sqlite3 /workdir/OpenPLC_v3/webserver/openplc.db "$SQL_DEVICE"
 /workdir/OpenPLC_v3/webserver/scripts/start_openplc.sh
