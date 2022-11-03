233f569180b8d31cb38def4d2dff975289bb328b
Lorenz Pircher <h1633083@wu.ac.at>
2022-11-03 12:39:07 +0100

    Update .bat File

--- setup.bat
+++ setup.bat
@@ -1,1 +1,17 @@
-docker volume create influxvolume && docker-compose up -d && cd factoryio/sdk && dotnet run
+@echo off 
+set /p vpn=Connected with vpn.univie.ac.at? (y/n):
+if %vpn%==n (echo\ && echo Please connect with the VPN. && exit)
+echo\
+set /p required=Is docker running and .NET Runtime and SDK preinstalled? (y/n):
+if %required%==n (echo\ && echo Please start Docker and make sure .NET Runtime and SDK are preinstalled. && exit)
+echo\
+echo Open FactoryI/O and if necessary, update the IP in the Repository (press any key to continue)
+pause > nul
+echo\ && echo Create Docker Volume for Influx if it does not exist.
+docker volume create influxvolume
+echo\ && echo Build and run Docker Container
+docker-compose up --build -d
+echo\ && echo Important: Open http://localhost:8080, upload the .st Program and start the PLC
+echo\ && echo Start Factory I/O SDK and Producer
+cd factoryio/sdk
+dotnet run
