09d03d0188254c03c1e5691c6778121c27702288
Lorenz Pircher <a01633083@unet.univie.ac.at>
2022-11-09 00:56:36 +0100

    Autostart

--- openplc/setup.sh
+++ openplc/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 SQL_SCRIPT="INSERT INTO Programs (Name, Description, File, Date_upload) VALUES ('Factory I/O Function Block Diagram', 'PLC Program for the Simulation', 'testprogramm.st', strftime('%s', 'now'));"
-SQL_DEVICE="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('FactoryIO', 'TCP', 1, '192.168.1.2', 503, 0, 16, 0, 16, 0, 8, 0, 8, 0, 8);"
+SQL_DEVICE="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('FactoryIO', 'TCP', 1, '${MODBUS_SERVER}', ${MODBUS_PORT}, 0, 16, 0, 16, 0, 8, 0, 8, 0, 8);"
 SQL_DNP="UPDATE Settings SET Value = 'disabled' WHERE Key = 'Dnp3_port';"
 SQL_ENIP="UPDATE Settings SET Value = 'disabled' WHERE Key = 'Enip_port'"
 SQL_DELETE="DELETE FROM Programs WHERE Name = 'Blank Program';"
@@ -17,5 +16,5 @@ sqlite3 /workdir/OpenPLC_v3/webserver/openplc.db "$SQL_DELETE"
 sqlite3 /workdir/OpenPLC_v3/webserver/openplc.db "$SQL_DNP"
 sqlite3 /workdir/OpenPLC_v3/webserver/openplc.db "$SQL_ENIP"
 sqlite3 /workdir/OpenPLC_v3/webserver/openplc.db "$SQL_AUTOST"
-
+cd /workdir/OpenPLC_v3
 /workdir/OpenPLC_v3/webserver/scripts/start_openplc.sh
--- setup.bat
+++ setup.bat
@@ -1,15 +1,19 @@
-@echo off 
+@echo off
+for /f "delims== tokens=1,2" %%G in (.env) do set %%G=%%H
 set /p vpn=Connected with vpn.univie.ac.at? (y/n):
 if %vpn%==n (echo\ && echo Please connect with the VPN. && exit)
 echo\
 set /p required=Is docker running and .NET Runtime and SDK preinstalled? (y/n):
 if %required%==n (echo\ && echo Please start Docker and make sure .NET Runtime and SDK are preinstalled. && exit)
 echo\
-echo Open FactoryI/O and if necessary, update the IP in the Repository (press any key to continue)
+echo Don't forget to upadte the MODBUS_SERVER IP in the .env File (press any key to continue)
 pause > nul
+echo\ && echo Starting Factory I/O ...
+start "" %FACTORY_IO%
 echo\ && echo Build and run Docker Container
 docker-compose up --build -d
-echo\ && echo Important: Open http://localhost:8080, upload the .st Program and start the PLC
+echo\ && echo Open Workflow Engine
+explorer "https://cpee.org/flow/?monitor=https://cpee.org/flow/engine/5768/"
 echo\ && echo Start Factory I/O SDK and Producer
 cd factoryio/sdk
 dotnet run
